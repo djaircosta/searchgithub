90c8cf4042f7e5c6f5700f5ecf5ab480d2674b17
Djair Costa <riajd10@gmail.com>
2022-06-07 07:00:08 -0300

    First commit

---
+++ src/main/java/com/daytech/searchgithub/SearchgithubApplication.java
@@ -0,0 +1,13 @@
+package com.daytech.searchgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchgithubApplication.class, args);
+	}
+
+}
